516dfe432474a5230587953d03faaca6ad35ef83
Toorq91 <157486467+Toorq91@users.noreply.github.com>
2024-11-21 13:48:59 +0100

    Bytte navn etter en viss tid + bruke metode

--- app.js
+++ app.js
@@ -1,21 +1,42 @@
-Vue.createApp({
+const vm = Vue.createApp({
     data() {
         return {
-            firstName: 'Test',
-            middleName: 'Testing',
-            lastName: 'Testington',
-            age: 894
+            firstName: 'Thor',
+            // middleName: 'Halvorsen',
+            lastName: 'Eikeland',
+            // age: 33,
+        }
+    },
+    methods: {
+        fullName() {
+            return `${this.firstName} ${this.lastName.toUpperCase()}`
         }
     }
 }).mount('#app')
 
-Vue.createApp({
-    data() {
-        return {
-            firstName: 'Test',
-            middleName: 'The',
-            lastName: 'Second',
-            age: 708
-        }
-    }
-}).mount('#app2')
+// setTimeout(() => {
+//     vm.firstName = 'Bob';
+// }, 3000);
+
+// setTimeout(() => {
+//     vm.middleName = '';
+// }, 3000);
+
+// setTimeout(() => {
+//     vm.lastName = 'Bobington';
+// }, 3000);
+
+// setTimeout(() => {
+//     vm.age = 45;
+// }, 3000);
+
+// Vue.createApp({
+//     data() {
+//         return {
+//             firstName: 'Test',
+//             middleName: 'The',
+//             lastName: 'Second',
+//             age: 708
+//         }
+//     }
+// }).mount('#app2')
