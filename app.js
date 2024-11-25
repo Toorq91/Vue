eb46cb0c09f913e8238cf61966a1857b4f51b3f3
Toorq91 <157486467+Toorq91@users.noreply.github.com>
2024-11-25 14:31:39 +0100

    Keyboard Events and Modifiers

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const vm = Vue.createApp({
     data() {
         return {
             firstName: 'Test',
+            middleName: '',
             lastName: 'Testington',
             url: 'https://google.com',
             raw_url: '<a href="https://google.com" target="_blank">Google</a>',
@@ -11,7 +12,7 @@ const vm = Vue.createApp({
     },
     methods: {
         fullName() {
-            return `${this.firstName} ${this.lastName.toUpperCase()}`
+            return `${this.firstName} ${this.middleName} ${this.lastName.toUpperCase()}`
         },
         increment() {
             this.age ++
@@ -20,6 +21,9 @@ const vm = Vue.createApp({
             console.log(msg)
             
             this.lastName = event.target.value
+        },
+        updateMiddleName(event) {
+            this.middleName = event.target.value
         }
     }
 }).mount('#app')
